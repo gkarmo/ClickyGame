9cf680af635f1e630f55a9083bf381d56c0ca017
Gayah Karmo <gayahkarmo@gmail.com>
2019-09-10 02:39:21 -0400

    code clean up/ photo's rendering

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -2,29 +2,23 @@ import React, { Component } from "react";
 import Card from "./components/Cards";
 import Wrapper from "./components/Wrapper";
 import card from "./cards.json"
-// import Title from "./components/Title";
-// import friends from "./friends.json";
 
 class App extends Component {
-  // // Setting this.state.friends to the friends json array
+
   state = {
     card
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
+        {this.state.card.map(cards => (
           <Card
-          
+            id={cards.id}
+            key={cards.id}
+            image={cards.image}            
           />
+        ))}
       </Wrapper>
     );
   }
--- testapp/src/components/Cards/index.js
+++ testapp/src/components/Cards/index.js
@@ -4,18 +4,6 @@ import React from "react";
 
 
 function Card(props) {
-    // Setting the initial state of the Counter component
-    //     state = {
-    //     images: []
-    //   };
-      
-      
-    //   componentDidMount = () => {
-    //     const images = imageLoader()
-    //     this.setState({images})
-    //   };
-    // The render method returns the JSX that should be rendered
-    // render() {
       return (
         <main class="container">
         <div role="img" aria-label="click item" class="click-item">
@@ -25,7 +13,6 @@ function Card(props) {
         </div>
       </main>
       );
-    // }
   }
 
 export default Card;
