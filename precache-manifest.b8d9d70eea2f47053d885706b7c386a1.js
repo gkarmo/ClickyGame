05c302ff618c3049c9ec63f1566fe383b12b5f76
Gayah Karmo <gayahkarmo@gmail.com>
2019-09-12 16:45:48 -0400

    Updates

---
+++ precache-manifest.b8d9d70eea2f47053d885706b7c386a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e94105a6bd3fc38af8ff65f573543b1",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "4217667b75a95fc609c9",
+    "url": "/ClickyGame/static/css/main.f29bb808.chunk.css"
+  },
+  {
+    "revision": "7da839f4b75a94fe6ba1",
+    "url": "/ClickyGame/static/js/2.840dcbc0.chunk.js"
+  },
+  {
+    "revision": "4217667b75a95fc609c9",
+    "url": "/ClickyGame/static/js/main.f8012df4.chunk.js"
+  },
+  {
+    "revision": "ce570810b44d43a6c210",
+    "url": "/ClickyGame/static/js/runtime~main.83d3ee16.js"
+  }
+]);
